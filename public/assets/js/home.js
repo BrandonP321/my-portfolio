9ffd707381d60faa8f8d4fe14ef4090c1aaf9681
Brandon Phillips <brandon.phillips976@gmail.com>
2020-10-31 00:50:21 -0700

    added scroll based functionality to navbar background

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,5 +1,6 @@
 // function to dynamically load in skill bars
 function loadSkills() {
+    // animate skill bar for each skill
     $('.skill-bar-filled').each(function () {
         // grab percent value from span element
         const percent = $(this).parent().siblings()[0].children[0].getAttribute('data-percent')
@@ -7,6 +8,7 @@ function loadSkills() {
         $(this).animate({ width: `${percent}%` }, 1000)
     })
 
+    // change skill percentage text for each skill
     $('.skill-percent').each(function (event) {
         // store current skill
         const skill = $(this)
@@ -16,13 +18,13 @@ function loadSkills() {
         let currentPercent = 0;
         // calculate interval speed to reach the given percent in 1000ms
         const intervalTime = 1000 / percent
-        console.log(currentPercent == percent)
         // create interval to count from 0 to skill's percent
         const percentInterval = setInterval(function () {
+            // if current percent equals skill's percent, clear interval
             if (currentPercent == percent) {
                 clearInterval(percentInterval)
             } else {
-                console.log(currentPercent, percent)
+                // else increment current percent and change percent text on page
                 currentPercent += 1
                 skill.text(`${currentPercent}%`)
             }
@@ -30,15 +32,47 @@ function loadSkills() {
     })
 }
 
+// variable indicating if white navbar background is shown or not
+let navBgIsShown = false
 // check if header needs to change as user scrolls
 $(window).on('scroll', function () {
     // if user has scrolled past 50 px from top of page, add class 'not-transparent' to navbar
-    if ($(this).scrollTop() > 200) {
-        $('nav').addClass('not-transparent')
+    if ($(this).scrollTop() > 56) {
+        // first check that white bg is not already shown
+        if (!navBgIsShown) {
+            navBgIsShown = true
+            $('#header-bg').animate({ height: '56px' }, 500)
+            // make navbar text black
+            $('nav').removeClass('navbar-dark')
+            $('nav').addClass('navbar-light')
+        }
     } else {
-        // else remove class from navbar
-        $('nav').removeClass('not-transparent')
+        // else remove class from navbar if white bg is already shown
+        if (navBgIsShown) {
+            $('#header-bg').animate({ height: '0' }, 500, function() {
+                // make navbar text white
+                $('nav').removeClass('navbar-light')
+                $('nav').addClass('navbar-dark')
+            })
+            navBgIsShown = false
+        }
     }
+})
+
+$('#header-bg').on('show.bs.collapse', function () {
+    console.log('will show')
+})
+
+$('#header-bg').on('shown.bs.collapse', function () {
+    console.log('is shown')
+})
+
+$('#header-bg').on('hide.bs.collapse', function () {
+    console.log('will hide')
+})
+
+$('#header-bg').on('hidden.bs.collapse', function () {
+    console.log('is hidden')
 })
 
 loadSkills()
